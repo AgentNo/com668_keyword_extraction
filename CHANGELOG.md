26bfbca878d1646654a4ae066b905b3c404b526d
AgentNo <dfaulkner2010@hotmail.co.uk>
2022-02-05 14:17:43 +0000

    Rewrote main logic, reduced logging, improved error handling

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+# v.1.2 [5th February 2022]
+- Completely estructured the logic to retrieve keywords and linked entites
+- Improved error handling to fix a crash relating to lack of entities
+- Reduced the amount of logging - keywords and entites are now shown in one log rather than a unique log per item
+- Fixed a comment typo 
+
 # v.1.1.1 [4th February 2022]
 - Added an addition log at the end of execution
 - Fixed a missing bracket which was causing a runtime error
--- deployment/getTextAnalyticsOutput/__init__.py
+++ deployment/getTextAnalyticsOutput/__init__.py
@@ -4,6 +4,7 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
 
+# Authentication instance of Language Cognitive Service
 def authenticate_client():
     ta_credential = AzureKeyCredential("680b4b1960da43e8a2b6f48029bb3d08")
     client = TextAnalyticsClient(
@@ -12,30 +13,67 @@ def authenticate_client():
     return client
 
 
-def abstract_analysis(client, title, abstract):
+# Return a list of all keywords mined from the abstract and the title. If an error occurs or no keywords are found, return an empty list 
+def get_paper_keywords(client, title, abstract):
     try:
         documents = [abstract, title]
         response = client.extract_key_phrases(documents = documents)[0]
 
-        if not response.is_error:
-            logging.info("\tKey Phrases:")
+        if not response.is_error and len(response.key_phrases) != 0:
+            keywords = []
             for phrase in response.key_phrases:
-                logging.info("\t\t" + phrase)
+                keywords.append(str(phrase).lower())
+            
+            logging.info("Found {} keywords: \n{}".format(len(keywords), keywords))
+            return keywords
         else:
-            logging.info(response.id, response.error)
-
-        result = client.recognize_linked_entities(documents = documents)[0]
-        logging.info("Linked Entities:\n")
-        for entity in result.entities:
-            logging.info("\tName: ", entity.name, "\tId: ", entity.data_source_entity_id, "\tUrl: ", entity.url, "\n\tData Source: ", entity.data_source)
-            logging.info("\tMatches:")
-            for match in entity.matches:
-                logging.info("\t\tText:{}".format(match.text))
-                logging.info("\t\tConfidence Score: {0:.2f}".format(match.confidence_score))
-                logging.info("\t\tOffset: {}".format(match.offset))
-                logging.info("\t\tLength: {}".format(match.length))
+            if response.error:
+                logging.info(response.id, response.error)
+            else:
+                logging.info('No keywords found in text!')
+            return []
     except Exception as err:
         print("Encountered exception: {}".format(err))
+        return []
+
+
+# Return a list of all linked entites found in the paper. If no entites are found or no keywords are found, return empty list
+def get_linked_entities(client, title, abstract):
+    try:
+        documents = [abstract, title]
+        response = client.recognize_linked_entities(documents = documents)[0]
+
+        if not response.is_error and len(response.entities) != 0:
+            logging.info("Found the following linked entities:\n")
+            entities = []
+            for entity in response.entities:
+                entity = dict()
+                matches = []
+                entity['name'] = str(entity.name)
+                entity['url'] = str(entity.url)
+                entity['data_source'] = str(entity.data_source)
+                for match in entity.matches:
+                    # TODO: Evaluate this and determine if any additional processing is needed to filter out useless links
+                    match = dict()
+                    match['text'] = str(match.text)
+                    match['confidence_score'] = str(match.confidence_score)
+                    match['offset'] = str(match.offset)
+                    match['length'] = str(match.length)
+                    matches.append(match)
+                entity['matches'] = matches
+                entities.append(entity)
+            logging.info("{}".format(entities))
+            return entities
+        else:
+            if response.error:
+                logging.info(response.id, response.error)
+                return []
+            else:
+                logging.info('No linked entities found in text!')
+            return []
+    except Exception as err:
+        print("Encountered exception: {}".format(err))
+        return []
 
 
 def main(documents: func.DocumentList) -> str:
@@ -45,7 +83,12 @@ def main(documents: func.DocumentList) -> str:
         for document in documents:
             # TODO: Check here to make sure that documents that are already processed don't get processed twice
             logging.info('Performing text analytics for document id: %s', document['id'])
-            title = document["title"]
-            abstract = document["abstract"]
-            abstract_analysis(client, title, abstract)
-            logging.info('Text analysis complete, writing output...')
+            if True:
+                title = document["title"]
+                abstract = document["abstract"]
+                logging.info('Searching for keywords...')
+                keywords = get_paper_keywords(client, title, abstract)
+                logging.info('Searching for linked entities...')
+                entites = get_linked_entities(client, title, abstract)
+        logging.info('Text analysis complete, writing output...')
+        # TODO: Write output to CosmosDB instance
